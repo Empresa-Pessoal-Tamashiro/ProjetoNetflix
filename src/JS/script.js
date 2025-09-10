ed1f9b9c6f98e4b1a1eb835a1579583850a870b9
Guilherme <guitama.games@gmail.com>
2025-09-10 10:59:01 -0300

    8- Concluindo o JS

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,22 +1,51 @@
-//declarando as variaveis com DOM (Document Model Object)
+//Array de objetos com as imagens e informações do banner
 
-const carroselLista = document.querySelector(".carrosel-lista")
-const carroselItem = document.querySelector(".carrosel-item")
-const btnAnterior = document.querySelector(".btn-anterior")
-const btnProximo = document.querySelector(".btn-proximo")
-let contador = 0;
+const bannerItems = [
+{
+    imagem:"./src/assets/img/Game1.jpg",
+    titulo: "Título do filme 1",
+    descricao:"Filme 1"
 
-const larguraItem = carroselItem[0].offsetWidth + 10;
-btnProximo.addEventListener("click",()=>{
-    if(contador <carroselItem.length -1){
-        contador++;
-        carroselLista.computedStyleMap.transform = "translate"(-$(contador * larguraItem))
+},
+{
+    imagem: "./src/assets/img/Game2.jpg",
+    titulo: "Título do filme 2",
+    descricao:"Filme 2"
+
+},
+{
+    imagem:"./src/assets/img/Game3.jpg",
+    titulo: "Título do filme 3",
+    descricao:"Filme 3"
+
+}
+];
+
+//Declarando as variáveis e elementos com DOM(document object model)
+
+const tempo =5000;
+const elementoBanner = document.querySelector(".banner");
+const elementoTitulo = document.querySelector(".banner-titulo");
+const elementoDescricao = document.querySelector(".banner-descricao");
+let i = 0;
+
+//função apra o slide show
+
+function slideShow(){
+    //altera a imagem de fundo do banner
+    elementoBanner.style.backgroundImage = `url(${bannerItems[i].imagem})`
+    // altera o título do banner
+    elementoTitulo.textContent = bannerItems[i].titulo;
+    elementoDescricao.textContent = bannerItems[i].descricao;
+
+
+    //incrementa o indice (i) e reinicia quando chegar no final
+
+    i++;
+    if (i >= bannerItems.length){
+        i = 0;
     }
-})
+    setTimeout(slideShow,tempo)
+}
 
-btnAnterior.addEventListener("click",()=>{
-    if(contador > 0){
-        contador--;
-        carroselLista.computedStyleMap.transform = "translate"(-$(contador * larguraItem))
-    }
-})
+slideShow();
